7ae4ad2ea87a3d7284fd414c9ddfc62d225f36cd
tosaken1116 <nagato20021122@gmail.com>
2023-08-20 00:09:41 +0900

    add:swagger

--- main.go
+++ main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"fmt"
 	"thresher/adapter/http/router"
+	_ "thresher/docs"
 	"thresher/infra"
-	"thresher/utils"
+	"thresher/utils/config"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title   Thresher
+// @version  1.0
+// @license.name Tosaken
+// @description This is Thresher hackathon backend.
+
+// @BasePath /api/v1
+// @securityDefinitions.basic BearerAuth
+// @in header
+// @name Authorization
 func main() {
 	infra.InitDatabase()
-	con := utils.LoadConfig()
+	con := config.LoadConfig()
 	addr := fmt.Sprintf(":%s", con.Srv.Port)
-	router := router.InitRouter()
+	r := router.InitRouter()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	router.Run(addr)
+	r.Run(addr)
 }
