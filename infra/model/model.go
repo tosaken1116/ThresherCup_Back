5a9f3a1ab5848077fd02e9fd2432d384fcfaa7c5
tosaken1116 <nagato20021122@gmail.com>
2023-08-26 16:25:23 +0900

    add:Message Model

--- infra/model/model.go
+++ infra/model/model.go
@@ -6,34 +6,34 @@ import (
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
+
 func (base *Base) BeforeCreate(tx *gorm.DB) (err error) {
 	base.ID = uuid.New()
 	return
 }
 
 func (p *Posts) AfterCreate(tx *gorm.DB) (err error) {
-    return tx.Model(p).Preload("User").Find(p).Error
+	return tx.Model(p).Preload("User").Find(p).Error
 }
 func (h *Home) AfterCreate(tx *gorm.DB) (err error) {
-    return tx.Model(h).Preload("User").Find(h).Error
+	return tx.Model(h).Preload("User").Find(h).Error
 }
 func (l *Location) AfterCreate(tx *gorm.DB) (err error) {
-    return tx.Model(l).Preload("User").Find(l).Error
+	return tx.Model(l).Preload("User").Find(l).Error
 }
 
-
 type Base struct {
-	ID        uuid.UUID `json:"id" gorm:"type:uuid;not null;primaryKey"`
+	ID uuid.UUID `json:"id" gorm:"type:uuid;not null;primaryKey"`
 }
 
 type Users struct {
-	ID        string `json:"id" gorm:"primary_key; not null"`
-	Name      string    `json:"name" gorm:"not null"`
-	ImageUrl  string    `json:"image_url" gorm:"not null"`
-	Email     string    `json:"email" gorm:"unique; not null"`
-	Description string `json:"description"`
-	CreatedAt time.Time `json:"created_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	ID          string    `json:"id" gorm:"primary_key; not null"`
+	Name        string    `json:"name" gorm:"not null"`
+	ImageUrl    string    `json:"image_url" gorm:"not null"`
+	Email       string    `json:"email" gorm:"unique; not null"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	DeletedAt   time.Time `json:"deleted_at"`
 
 	Likes       []Posts   `json:"likes" gorm:"many2many:likes"`
 	Following   []Users   `json:"following" gorm:"many2many:following; foreignKey:ID;association_foreignKey:ID;joinForeignKey:followed_id;JoinReferences:following_id"`
@@ -47,36 +47,45 @@ type Posts struct {
 	Description string    `json:"description" gorm:"not null"`
 	CreatedAt   time.Time `json:"created_at"`
 	DeletedAt   time.Time `json:"deleted_at"`
-	UserID      string `json:"user_id" gorm:"not null"`
+	UserID      string    `json:"user_id" gorm:"not null"`
 
 	User  Users   `json:"user"`
 	Liked []Users `json:"liked" gorm:"many2many:likes"`
 }
 
 type Home struct {
-	UserID       string `json:"user_id" gorm:"primaryKey; not null"`
-	Longitude    float32    `json:"longitude" gorm:"not null"`
-	Latitude     float32    `json:"latitude" gorm:"not null"`
-	NonPassRange uint16    `json:"non_pass_range" gorm:"not null"`
+	UserID       string  `json:"user_id" gorm:"primaryKey; not null"`
+	Longitude    float32 `json:"longitude" gorm:"not null"`
+	Latitude     float32 `json:"latitude" gorm:"not null"`
+	NonPassRange uint16  `json:"non_pass_range" gorm:"not null"`
 
 	User Users `json:"user"`
 }
 
 type Location struct {
 	Base
-	Longitude float32    `json:"longitude" gorm:"not null"`
-	Latitude  float32    `json:"latitude" gorm:"not null"`
+	Longitude float32   `json:"longitude" gorm:"not null"`
+	Latitude  float32   `json:"latitude" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
-	UserID    string `json:"user_id" gorm:"not null"`
+	UserID    string    `json:"user_id" gorm:"not null"`
 
 	User Users `json:"user"`
 }
 
 type Encounter struct {
 	Base
-	Longitude float32    `json:"longitude" gorm:"not null"`
-	Latitude  float32    `json:"latitude" gorm:"not null"`
+	Longitude float32   `json:"longitude" gorm:"not null"`
+	Latitude  float32   `json:"latitude" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
-	PassingId string `json:"passing_id" gorm:"not null"`
-	PassedId  string `json:"passed_id" gorm:"not null"`
+	PassingId string    `json:"passing_id" gorm:"not null"`
+	PassedId  string    `json:"passed_id" gorm:"not null"`
+}
+
+type Message struct {
+	Base
+	SenderId    string    `json:"sender_id" gorm:"not null"`
+	ResponderId string    `json:"responder_id" gorm:"not null"`
+	Content     string    `json:"content" gorm:"not null"`
+	IsRead      bool      `json:"is_read" gorm:"not null; default:false"`
+	CreatedAt   time.Time `json:"created_at"`
 }
